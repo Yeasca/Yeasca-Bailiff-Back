72dd47fa7a8c7d034f1bcea8785863ba1763936a
Emeric <egaichet@gmail.com>
2014-01-16 13:36:37 +0100

    Logs

---
+++ Yeasca/Yeasca.TestsUnitaires/Metier/Outils/Logs/TestLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yeasca.Metier;
+
+namespace Yeasca.TestsUnitaires.Metier.Outils.Logs
+{
+    [TestClass]
+    public class TestLog
+    {
+        [TestMethod]
+        public void TestLog_peutLoggerUnMessage()
+        {
+            Log.loguer("pouet", new Exception());
+            string cheminLog = Log.cheminDuLog();
+
+            Assert.IsTrue(File.Exists(cheminLog));
+
+            string texte = File.ReadAllText(cheminLog);
+            Assert.IsTrue(texte.Contains("pouet"));
+
+            File.Delete(cheminLog);
+        }
+    }
+}
